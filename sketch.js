9d7bb3436963c611bdc92e890e529d1f0254bb7c
Tierra Smith <tsmith@wlapcs.org>
2017-03-02 20:46:38 +0000

    added shape

--- sketch.js
+++ sketch.js
@@ -1,20 +1,25 @@
-var newWidth = 100;
-var newHeight = 100;
+var newWidth = 50;
+var newHeight = 50;
 
 
 //setup function executes once 
 function setup(){
     createCanvas(600,400);
-    background(65,244,94);
+    background(244,65,208);
 }
 
 //draw function loops forever 
 function draw(){
+    var diameter = random(1,250);
+    frameRate(60);
    // background(50);
    if(mouseIsPressed){
        noStroke();
-    fill(244,65,121,50);
-    ellipse(mouseX,mouseY,newWidth,newHeight);
+    fill(random(100,255),65, 71, 244);
+    triangle(mouseX,mouseY-50,mouseX-50,mouseY+50,mouseX+50,mouseY+50);
+   
+    fill(random(0,255),random(0,255),random(0,255));
+    ellipse(mouseX,mouseY,diameter, diameter);       
    }
    
 }
